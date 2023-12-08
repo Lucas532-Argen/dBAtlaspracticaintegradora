1913afd99c68c5507e0d8f80327e4be9db6dca2e
Lucas Armaretti <larmaretti@gmail.com>
2023-12-07 22:26:41 -0300

    fixed

--- src/db/config.js
+++ src/db/config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const URI =
-  "mongodb+srv://larmaretti:<9kcb7FpMKNCd++u>@clusterlucasarma.4j2oryz.mongodb.net/?retryWrites=true&w=majority";
-
+  //"mongodb+srv://larmaretti:<9kcb7FpMKNCd++u>@clusterlucasarma.4j2oryz.mongodb.net/ecommerceLA?retryWrites=true&w=majority";
+    "mongodb://localhost:27017/ecommerceLA"
 mongoose
   .connect(URI)
   .then(() => console.log("conectado a bd"))
